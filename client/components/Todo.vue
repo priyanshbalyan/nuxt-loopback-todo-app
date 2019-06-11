dda19280954e5d5a15526b5daa278eeaaf0062a4
BALYAN PRIYANSH - MFCSL <23246311@mahindra.com>
2019-06-11 16:00:37 +0530

    Added edit todo feature

--- client/components/Todo.vue
+++ client/components/Todo.vue
@@ -11,15 +11,32 @@
         <section v-else>
             <div v-for="(todo, index) in todos" v-bind:key="todo.id">
                 <v-card width=500>
+                    <section v-if="edititem && index==edititem.index">
+                      <v-card-title>
+                        <v-text-field v-model="edititem.title" label="Title" type="text" />
+                      </v-card-title>
+                      <v-card-text>
+                        <v-text-field v-model="edititem.description" label="Description" type="text" />
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-btn color='yellow' @click='editTodo(edititem.id, edititem.index, edititem.title, edititem.description)'>EDIT TODO</v-btn>
+                        <v-btn color='red' @click='edititem = null'>CANCEL</v-btn>
+                      </v-card-actions>
+                    </section>
+                    <section v-else>
                     <v-card-title class="headline">{{ todo.title }}</v-card-title>
                     <v-card-text>
-                        <p>{{ todo.description }}</p>
+                      <p>{{ todo.description }}</p>
                     </v-card-text>
                     <v-card-actions>
-                        <v-checkbox @click='editStatus(todo.id, index, todo.isComplete)' color='pink' v-model="todo.isComplete" v-bind:label='todo.isComplete ? "Completed":"Incomplete"'></v-checkbox>
+                        <v-checkbox @click='changeStatus(todo.id, index, todo.isComplete)' color='pink' v-model="todo.isComplete" v-bind:label='todo.isComplete ? "Completed":"Incomplete"'></v-checkbox>
                         <v-spacer></v-spacer>
-                        <!-- <v-btn flat color="yellow" icon @click="edit(todo.id)"><v-icon>edit</v-icon></v-btn> -->
-                        <v-tooltip right><template v-slot:activator="{ on }">
+                        <v-tooltip top><template v-slot:activator="{ on }">
+                        <v-btn v-on="on" flat color="yellow" icon @click="showEditBox(todo.id, index)"><v-icon>edit</v-icon></v-btn>
+                        </template>
+                        <span>Edit</span>
+                        </v-tooltip>
+                        <v-tooltip top><template v-slot:activator="{ on }">
                           <v-btn
                           flat
                           color="red"
@@ -31,6 +48,7 @@
                         <span>Delete</span>
                         </v-tooltip>
                     </v-card-actions>
+                    </section>
                 </v-card>
                 <v-divider />
             </div>
@@ -46,7 +64,7 @@ var BASE_URL = process.env.BASE_URL
 export default {
   name: 'Todo',
   data() {
-    return { todos: [], loading: false, error: null }
+    return { todos: [], loading: false, error: null, edititem:null }
   },
   mounted() {
     console.log('mounted')
@@ -75,9 +93,19 @@ export default {
         this.todos.splice(index, 1)
       });
     },
-    editStatus(id, index, status){
+    changeStatus(id, index, status){
       axios.patch(BASE_URL + '/api/todos/'+id, { isComplete: !status }).catch(err=>console.log(err)).finally(()=>{
         this.todos[index].isComplete = !status
+      });
+    },
+    showEditBox(id, index){
+      this.edititem = { id: id, index: index, title: this.todos[index].title, description: this.todos[index].description }
+    },
+    editTodo(id, index, title, description){
+      axios.patch(BASE_URL + '/api/todos/'+id, {title:title, description:description}).catch(err=>console.log(err)).finally(()=>{
+        this.edititem = null
+        this.todos[index].title = title
+        this.todos[index].description = description
       });
     }
   }
